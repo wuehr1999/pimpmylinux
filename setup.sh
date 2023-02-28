d7e283e3920e8f937c5c3e4fd766752c4144cf08
wuehr1999 <jonas.wuehr@t-online.de>
2023-02-28 21:03:22 +0100

    remove setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install clang-format
-git clone https://github.com/rhysd/vim-clang-format
-mkdir ~/.vim
-cp -r vim-clang-format/autoload vim-clang-format/doc vim-clang-format/plugin ~/.vim
-cp .vimrc ~/
